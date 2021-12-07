16565a9835ae243e127f6de60b090b128f08b5b4
chen <chen665@gmail.com>
2021-12-08 00:07:22 +0200

    Home page changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React , { useState } from 'react';
 import Text from "components/Text";
 import UserList from "components/UserList";
 import { usePeopleFetch } from "hooks";
 import * as S from "./style";
+import TabPanel from '@material-ui/lab/TabPanel';
+import Cookies from 'js-cookie';
 
-const Home = () => {
+const Home = ({getStoredFavoriteUsers,setNavValue}) => {
   const { users, isLoading } = usePeopleFetch();
-
+  setNavValue(0);
   return (
     <S.Home>
       <S.Content>
